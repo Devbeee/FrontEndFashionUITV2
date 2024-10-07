2edcd51be8f1f6b3909a37ad1ccf8a791658f19a
QuocDuy03 <quocduyy.03@gmail.com>
2024-10-07 23:19:07 +0700

    create: home page, product component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,52 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors:{
-
+      colors: {
         'blue-cyan': '#00354b',
-        'yellow':'#ffc107',
+        'yellow': '#ffc107',
         'primary': '#007bff'
       },
       width: {
         '1200': '1200px'
+      },
+      willChange: {
+        'opacity': 'opacity'
+      },
+      keyframes: {
+        pulsate: {
+          '0%': {
+            transform: 'scale(0.1)',
+            opacity: '0',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'scale(1.2)',
+          },
+        },
+        aniName: {
+          '0%': {
+            left: '0px'
+          },
+
+          '50%': {
+            left: '3%',
+          },
+
+          '100%': {
+            left: '0px'
+          }
+        }
+      },
+      animation: {
+        pulsate: 'pulsate 1s ease-out infinite',
+        aniName: 'aniName 3s infinite'
+      },
+      backgroundImage: {
+        'fire': "url('@/assets/images/fire.png')",
+        'big-banner-sale': "url('@/assets/images/bg_banner_big.webp')"
       }
     }
   },
