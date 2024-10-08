ff7b5a14bafb07cf2c9c1f95d87421687ad91811
seosau <seosausau@gmail.com>
2024-10-08 13:21:26 +0700

    add: example news image, tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,16 @@ export default {
 
         'blue-cyan': '#00354b',
         'yellow':'#ffc107',
-        'primary': '#007bff'
+        'primary': '#007bff',
+        'gray100': '#f3f4f6' ,
+        'gray200':'#e5e7eb ',
+        'gray600': '#4b5563 ',
+        'white-primary': '#ffffff'
       },
       width: {
+        '1200': '1200px'
+      },
+      maxWidth: {
         '1200': '1200px'
       }
     }
