fa1f08d70f17fd45ea44bab1c48b680020bc047e
seosau <seosausau@gmail.com>
2024-10-10 10:45:24 +0700

    update: import and use blog interface for blog page and related comp

--- src/components/HotNews.tsx
+++ src/components/HotNews.tsx
@@ -1,29 +1,25 @@
 import { Link } from 'react-router-dom';
 import { Typography } from 'antd';
 import news_Img from '@/assets/images/news_Img.webp'
+import { IHotNews } from '@/interfaces';
 
 const { Title, Paragraph } = Typography;
 
 type HotNewsProps = {
-  newsItems: Array<{
-    title: string;
-    link: string;
-    date: string;
-    image: string;
-  }>;
+  newsItems: IHotNews[];
 };
 
 export function HotNews ({ newsItems } : HotNewsProps) {
   return (
-    <div className="rounded-lg border border-gray200 p-4 shadow-md mb-5">
-      <Title level={5} className="text-base uppercase font-bold mb-2 border-b border-gray200 pb-1">
-        <Link to="/blogs/all" title="Tin tức nổi bật" className="">
+    <div className="rounded-lg border border-gray-200 p-4 shadow-md mb-5">
+      <Title level={5} className="text-base uppercase font-bold mb-2 border-b border-gray-200 pb-1">
+        <Link to="/blogs" title="Tin tức nổi bật" className="">
           <span className='text-blue-cyan hover:text-primary'>Tin tức nổi bật</span>
         </Link>
       </Title>
       <div>
         {newsItems.map((item, index) => (
-          <div key={index} className="flex mb-3 border-b border-dashed border-gray200 pb-2">
+          <div key={index} className="flex mb-3 border-b border-dashed border-gray-200 pb-2">
             <div className="w-24 mr-3">
               <Link to={item.link} title={item.title} className="block">
                 <img
@@ -39,7 +35,7 @@ export function HotNews ({ newsItems } : HotNewsProps) {
                   {item.title}
                 </Link>
               </h3>
-              <Paragraph className="text-xs text-left italic text-gray600 mb-0">{item.date}</Paragraph>
+              <Paragraph className="text-xs text-left italic text-gray-600 mb-0">{item.date}</Paragraph>
             </div>
           </div>
         ))}
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -3,17 +3,19 @@ import { Typography } from 'antd';
 
 const { Title } = Typography;
 
+type Tag = {
+  name: string;
+  link: string;
+}
+
 type TagsProps = {
-  tags: Array<{
-    name: string;
-    link: string;
-  }>;
+  tags: Tag[];
 };
 
 export function Tags ({ tags } : TagsProps) {
   return (
-    <div className="rounded-lg border border-gray200 p-4 shadow-md mb-5">
-      <Title level={5} className="text-blue-cyan text-sm uppercase font-bold mb-2 border-b border-gray200 pb-1">
+    <div className="rounded-lg border border-gray-200 p-4 shadow-md mb-5">
+      <Title level={5} className="text-blue-cyan text-sm uppercase font-bold mb-2 border-b border-gray-200 pb-1">
         Tags
       </Title>
       <ul className="p-0 list-none flex flex-wrap gap-3">
@@ -22,7 +24,7 @@ export function Tags ({ tags } : TagsProps) {
             <Link
               to={tag.link}
               title={tag.name}
-              className="text-gray-800 transition duration-300 ease-in-out px-2 py-1 border border-gray200 m-1 rounded hover:bg-blue-cyan hover:border-blue-cyan hover:text-white-primary"
+              className="text-gray-800 transition duration-300 ease-in-out px-2 py-1 border border-gray-200 m-1 rounded hover:bg-blue-cyan hover:border-blue-cyan hover:text-white"
             >
               {tag.name}
             </Link>
