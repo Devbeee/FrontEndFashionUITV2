a962e6626383a8306da792487941aad162451118
Truong <truonghs2903@gmail.com>
2024-10-15 18:40:49 +0700

    fix: tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,51 @@ export default {
       },
       screens: {
         xs: '480px'
+      },
+      willChange: {
+        opacity: 'opacity'
+      },
+      keyframes: {
+        pulsate: {
+          '0%': {
+            transform: 'scale(0.1)',
+            opacity: '0'
+          },
+          '50%': {
+            opacity: '1'
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'scale(1.2)'
+          }
+        },
+        aniName: {
+          '0%': {
+            left: '0px'
+          },
+
+          '50%': {
+            left: '3%'
+          },
+
+          '100%': {
+            left: '0px'
+          }
+        },
+        shine: {
+          '100%': {
+            left: '125%'
+          }
+        }
+      },
+      animation: {
+        pulsate: 'pulsate 1s ease-out infinite',
+        aniName: 'aniName 3s infinite',
+        shine: 'shine 1.1s'
+      },
+      backgroundImage: {
+        fire: "url('@/assets/images/fire.png')",
+        'big-banner-sale': "url('@/assets/images/bg_banner_big.webp')"
       }
     }
   },
