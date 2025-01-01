e3f35eea939058767b4667a4e9ceb08ba9dc161b
Devbeee <tngb1002@gmail.com>
2025-01-01 23:55:45 +0700

    fix bug deploy 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -87,7 +89,7 @@ export default {
     }
   },
   plugins: [
-    require('tailwindcss-animate'),
+    tailwindcssAnimate,
     function ({ addUtilities }) {
       addUtilities({
         '.no-scrollbar': {
