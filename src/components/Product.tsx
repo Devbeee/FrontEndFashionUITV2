bb69b16185bb513fe41835125476927438f09fc3
seosau <seosausau@gmail.com>
2025-01-04 12:18:57 +0700

    fix product comp

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -17,7 +17,7 @@ export const Product: React.FC<ProductProps> = ({ product, handleClickEye }) => 
           <div className='relative overflow-hidden group'>
             <div className='absolute w-full h-full bg-gray-300 opacity-0 z-0 top-0 right-0 ease-in-out duration-150 group-hover:opacity-70'></div>
               <img
-                className='bg-transparent w-auto rounded md::h-[300px] h-[250px] top-0 bottom-0 m-auto ease-in-out duration-300 will-change-opacity'
+                className='bg-transparent w-auto rounded md:h-[300px] h-[250px] top-0 bottom-0 m-auto ease-in-out duration-300 will-change-opacity'
                 src={product?.productDetails[0]?.imgUrl}
                 alt={product?.name}
               />
